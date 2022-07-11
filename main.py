37d2117bbfd95b1db8b03d4ca70bc2b2e3f21ae1
rasyidev <habib.rasyid11@gmail.com>
2022-07-11 16:27:00 +0700

    Add infinix scraped data

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ urls = [
 
 # s.offline_scrape("scraped_data/14/File.html")
 # import time
-for i in range(598,616):
+for i in range(616,715):
   # print(i)
   s.offline_scrape(f"scraped_data/{i}/File.html")
 
